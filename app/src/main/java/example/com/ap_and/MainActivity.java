9b721549051efef074764d72e1a99ce68f5f7b2e
James Durham <jamesldurham@gmail.com>
2016-09-22 20:53:00 -0500

    rest init

---
+++ app/src/main/java/example/com/ap_and/MainActivity.java
@@ -0,0 +1,12 @@
+package example.com.ap_and;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
